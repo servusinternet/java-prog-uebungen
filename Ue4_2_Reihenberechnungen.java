587555f575eb86dfe81451ea2323557eb072c977
servusinternet <ph@servusinternet.de>
2014-11-10 20:14:49 +0100

    Create Ue4_2_Reihenberechnungen.java

---
+++ Ue4_2_Reihenberechnungen.java
@@ -0,0 +1,17 @@
+public class Ue4_2_Reihenberechnung {
+	public static void main(String[] args) {
+		
+		double summe = 0;
+		double aenderung = 1;
+		double x = 1/Math.pow(10, 5);
+				
+		for (int k = 1 ; aenderung >= x; k++) {
+			aenderung = 1/(double)(k*k);
+			summe = summe + aenderung;
+			System.out.println(k + ". Durchlauf: 6*" + summe + " = " + 6*summe);
+			//System.out.println(aenderung);
+		}
+		
+	}
+	
+}
