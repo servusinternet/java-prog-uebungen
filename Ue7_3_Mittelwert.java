0020b7522de51441c9ca25e3d4e32dc08059a12c
servusinternet <ph@servusinternet.de>
2014-11-30 14:01:52 +0100

    Ue7_3_Mittelwert

---
+++ Ue7_3_Mittelwert.java
@@ -0,0 +1,33 @@
+package prog.Uebungen;
+
+import java.util.Scanner;
+
+public class Ue7_3_Mittelwert {
+	public static void main(String[] args) {
+		double groessterWert = -1;
+		double kleinsterWert = -1;
+		double ergebnis = 0;
+		
+		Scanner eingabe = new Scanner(System.in);
+		System.out.println("Anzahl Werte:");
+		int n = eingabe.nextInt();
+		
+		for (int i=1; i <= n; i++) {
+			System.out.print("Wert x" + i + ": ");
+			double x = eingabe.nextDouble();
+			if (kleinsterWert == -1) {
+				kleinsterWert = x;
+			} else if (kleinsterWert > x) {
+				kleinsterWert = x;
+			}
+			if (x > groessterWert) {
+				groessterWert = x;
+			}
+			ergebnis = ergebnis + x;
+		}
+		System.out.println("Größter Wert: " + groessterWert);
+		System.out.println("Kleinster Wert: " + kleinsterWert);
+		System.out.println("Arithm. Mittelwert: " + ergebnis/n);
+		eingabe.close();
+	}
+}
